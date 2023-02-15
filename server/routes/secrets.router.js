bd73f8e7fb9e2eb09ac14594d594fe7dee324c7a
Brock Benson <brockjbenson@outlook.com>
2023-02-15 12:52:16 -0600

    all base done

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -9,13 +9,26 @@ router.get("/", rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log("req.user:", req.user);
 
-  pool
-    .query(`SELECT * FROM "secret";`)
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log("Error making SELECT for secrets:", error);
-      res.sendStatus(500);
-    });
+  if (req.user.clearance_level >= 13) {
+    pool
+      .query(`SELECT * FROM "secret";`)
+      .then((results) => res.send(results.rows))
+      .catch((error) => {
+        console.log("Error making SELECT for secrets:", error);
+        res.sendStatus(500);
+      });
+  } else {
+    let queryText = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1`;
+    pool
+      .query(queryText, [req.user.clearance_level])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  }
 });
 
 module.exports = router;
