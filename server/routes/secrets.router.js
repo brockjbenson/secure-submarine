52949d299da753e2439bf9a3e625464369226aef
Brock Benson <brockjbenson@outlook.com>
2023-02-15 12:41:30 -0600

    first base done

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+const pool = require("../modules/pool");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
-  console.log('req.user:', req.user);
+  console.log("req.user:", req.user);
 
   pool
     .query(`SELECT * FROM "secret";`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
+      console.log("Error making SELECT for secrets:", error);
       res.sendStatus(500);
     });
 });
